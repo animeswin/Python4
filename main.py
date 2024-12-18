3bf9c9dca243ba5e5ea17fd136862ad6f6ffdc30
animeswin <163599618+animeswin@users.noreply.github.com>
2024-12-18 11:46:14 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+def media(num1, num2, num3):
+    soma = num1 + num2 + num3
+    media_aritmetica = soma / 3
+    return media_aritmetica
+
+resultado = media(50, 60, 70)
+print(f"A média aritmética é: {resultado}")
